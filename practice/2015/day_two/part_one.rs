a3e4dc16018ca87ff72d6571d68973518a308851
Peter Kroeger <peter14mail@gmail.com>
2025-11-25 12:49:21 -0500

    2015 day 2

--- practice/2015/day_two/part_one.rs
+++ practice/2015/day_two/part_one.rs
@@ -1,5 +1,52 @@
+use std::{cmp::min, thread::{self, JoinHandle}};
+
 const DATA: &str = include_str!("input.txt");
 
+
+fn smallest_dim(x: u16, y: u16, z: u16) -> u16 {
+    min(x, min(y, z))
+}
+
+fn get_areas(l: u16, w:u16, h: u16) -> Vec<u16> {
+    vec![l * w, w * h, h * l]
+}
+
+fn get_surface_area(l: u16, w:u16, h: u16) -> u16 {
+    (2 * l * w) +  (2 * w * h) + (2 * h * l)
+}
+
+fn dims_to_surface_area(dim: &str) -> u16 {
+    let split_str: Vec<&str> = dim.split("x").collect();
+
+    if split_str.len() < 3 || split_str.len() > 3 {
+        return 0;
+    }
+
+    let l = split_str[0].parse::<u16>().unwrap();
+    let w = split_str[1].parse::<u16>().unwrap();
+    let h = split_str[2].parse::<u16>().unwrap();
+
+    let areas = get_areas(l, w, h);
+
+    get_surface_area(l, w, h) + smallest_dim(areas[0], areas[1], areas[2])
+}
+
 fn main() {
-    todo!()
+    let mut handles: Vec<JoinHandle<u16>> = Vec::new();
+
+    for dim_list in DATA.split_whitespace() {
+        let handle = thread::spawn(move || {
+            dims_to_surface_area(dim_list)
+        });
+
+        handles.push(handle);
+    }
+
+    let mut sum: u32 = 0;
+
+    for handle in handles {
+        sum += handle.join().unwrap() as u32;
+    }
+
+    println!("{}", sum)
 }
--- practice/2015/day_two/part_two.rs
+++ practice/2015/day_two/part_two.rs
@@ -1,5 +1,44 @@
 const DATA: &str = include_str!("input.txt");
 
+
+fn smallest_2_dims(x: u16, y: u16, z: u16) -> (u16, u16) {
+    let mut sorted_vec = vec![x, y, z];
+
+    sorted_vec.sort();
+
+    (sorted_vec[0], sorted_vec[1])
+}
+
+fn get_smallest_perimeter(l: u16, w:u16, h: u16) -> u16 {
+    let (x, y) = smallest_2_dims(l, w, h);
+
+    (2 * x) + (2 * y)
+}
+
+fn get_volume(l: u16, w:u16, h: u16) -> u16 {
+    l * w * h
+}
+
+fn dims_to_surface_area(dim: &str) -> u16 {
+    let split_str: Vec<&str> = dim.split("x").collect();
+
+    if split_str.len() < 3 || split_str.len() > 3 {
+        return 0;
+    }
+
+    let l = split_str[0].parse::<u16>().unwrap();
+    let w = split_str[1].parse::<u16>().unwrap();
+    let h = split_str[2].parse::<u16>().unwrap();
+
+    get_volume(l, w, h) + get_smallest_perimeter(l, w, h)
+}
+
 fn main() {
-    todo!()
+    let mut sum: u32 = 0;
+
+    for dim_list in DATA.split_whitespace() {
+        sum += dims_to_surface_area(dim_list) as u32;
+    }
+
+    println!("{}", sum)
 }
