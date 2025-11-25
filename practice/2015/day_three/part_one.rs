306cd6e73a9a40711ebd2b6b2a940b591161bb3e
Peter Kroeger <peter14mail@gmail.com>
2025-11-25 17:27:06 -0500

    2015 day 3

--- practice/2015/day_three/part_one.rs
+++ practice/2015/day_three/part_one.rs
@@ -1,6 +1,29 @@
+use std::collections::HashSet;
+
 const DATA: &str = include_str!("input.txt");
 
 
+fn make_move(direction: char) -> (i8, i8) {
+    match direction {
+        '^' => (0, 1),
+        'v' => (0, -1),
+        '>' => (1, 0),
+        '<' => (-1, 0),
+        other => panic!("Unknown character {other}")
+    }
+}
+
 fn main() {
-    todo!()
+    let mut position: (i8, i8) = (0, 0);
+    let mut a: HashSet<(i8, i8)> = vec![position].into_iter().collect();
+
+    DATA.chars().for_each(|f| {
+        let new_pos = make_move(f);
+
+        position = (position.0 + new_pos.0, position.1 + new_pos.1);
+
+        a.insert(position.clone());
+    });
+
+    println!("{}", a.len())
 }
--- practice/2015/day_three/part_two.rs
+++ practice/2015/day_three/part_two.rs
@@ -1,6 +1,40 @@
+use std::collections::HashSet;
+
 const DATA: &str = include_str!("input.txt");
 
 
+fn make_move(direction: char) -> (i8, i8) {
+    match direction {
+        '^' => (0, 1),
+        'v' => (0, -1),
+        '>' => (1, 0),
+        '<' => (-1, 0),
+        other => panic!("Unknown character {other}")
+    }
+}
+
 fn main() {
-    todo!()
+    let mut santa_position: (i8, i8) = (0, 0);
+    let mut robot_position: (i8, i8) = (0, 0);
+
+    let mut santa_visits: HashSet<(i8, i8)> = vec![].into_iter().collect();
+    let mut robot_visits: HashSet<(i8, i8)> = vec![].into_iter().collect();
+
+    DATA.chars().collect::<Vec<char>>().chunks(2).for_each(
+        |f| {
+            println!("{} {}", f[0], f[1]);
+            let santa_move = make_move(f[0]);
+            let robot_move = make_move(f[1]);
+
+            santa_position = (santa_position.0 + santa_move.0, santa_position.1 + santa_move.1);
+            robot_position = (robot_position.0 + robot_move.0, robot_position.1 + robot_move.1);
+
+            santa_visits.insert(santa_position.clone());
+            robot_visits.insert(robot_position.clone());
+        }
+    );
+
+    let diff = robot_visits.intersection(&santa_visits).count();
+
+    println!("{}", santa_visits.len() + robot_visits.len() - diff);
 }
