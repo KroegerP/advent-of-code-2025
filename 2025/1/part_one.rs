1e97383cfe4b8f9433b8540be98d832ceaa7c939
Peter Kroeger <peter14mail@gmail.com>
2025-12-01 14:25:30 -0500

    2025 part 1

--- 2025/1/part_one.rs
+++ 2025/1/part_one.rs
@@ -1,3 +1,34 @@
+const DATA: &str = include_str!("input.txt");
+
+fn get_clicks(rotation: &str) -> i32 {
+    let (dir, count) = rotation.split_at(1);
+
+    let clicks: i32 = count.parse().unwrap();
+
+    print!("{} {} |     ", dir, clicks);
+
+    if dir == "R" { clicks } else { clicks * -1 }
+}
+
 fn main() {
-    todo!();
+    let mut current_position = 50;
+
+    let mut zero_counter = 0;
+
+    DATA.split_whitespace().enumerate().for_each(|(i, f)| {
+        let clicks = get_clicks(f);
+
+        print!("{} + {} = ", current_position, clicks);
+
+        current_position = ((current_position + clicks) % 100 + 100) % 100;
+
+        println!("{}, ", current_position);
+
+        if current_position == 0 {
+            zero_counter += 1;
+            println!("Went to zero at index {} {}", i, f);
+        }
+    });
+
+    println!("Went to zero {} times", zero_counter);
 }
