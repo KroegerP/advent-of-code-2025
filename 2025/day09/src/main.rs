35f1cfd4b07aecc014e497a6cfcb0a43582218e4
Peter Kroeger <peter14mail@gmail.com>
2025-12-09 18:37:54 -0800

    refactor: brute force day 9 part 1

--- 2025/day09/src/main.rs
+++ 2025/day09/src/main.rs
@@ -1,15 +1,12 @@
-use utils::get_input;
+use itertools::Itertools;
 
-fn calc_area(c1: &(i64, i64), c2: &(i64, i64)) -> u64 {
-    // println!("Calculating {:?} {:?}", c1, c2);
-    (((c2.1 - c1.1).abs() + 1) * ((c2.0 - c1.0).abs() + 1)) as u64
-}
+use utils::get_input;
 
 fn main() {
     // let data = include_str!("test.txt");
     let data = get_input(2025, 9);
 
-    let mut coordinates: Vec<(i64, i64)> = data
+    let coordinates: Vec<(i64, i64)> = data
         .split_whitespace()
         .map(|f| {
             let mut xy = f.split(",");
@@ -21,35 +18,17 @@ fn main() {
         })
         .collect();
 
-    coordinates.sort_by_key(|a| (a.0, a.1));
+    let mut max = 0;
 
-    // println!("{:?}", coordinates);
-
-    let mut l = 0;
-    let mut r = coordinates.len() - 1;
-
-    let mut max: u64 = 0;
-
-    while r > l {
-        // println!("Trying {:?} {:?}", coordinates[l], coordinates[r]);
-
-        let new_calc: u64 = calc_area(&coordinates[l], &coordinates[r]);
-
-        // println!("{new_calc}");
-
-        if new_calc > max {
-            println!("New Max: {new_calc}");
-            max = new_calc;
-        }
-
-        let next_l_area: u64 = calc_area(&coordinates[l + 1], &coordinates[r]);
-        let next_r_area: u64 = calc_area(&coordinates[l], &coordinates[r - 1]);
-
-        if next_l_area > next_r_area {
-            l += 1;
-        } else {
-            r -= 1;
-        }
+    for pair in coordinates.iter().combinations(2) {
+        let [&p1, &p2] = pair[0..2] else {
+            unreachable!()
+        };
+        let x1 = p1.0.min(p2.0);
+        let x2 = p1.0.max(p2.0);
+        let y1 = p1.1.min(p2.1);
+        let y2 = p1.1.max(p2.1);
+        max = max.max((x2 - x1 + 1) * (y2 - y1 + 1));
     }
 
     println!("{max}");
