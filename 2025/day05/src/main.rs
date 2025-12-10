01adade512c2604322e96be2401c093fc404fdb7
Peter Kroeger <peter14mail@gmail.com>
2025-12-09 18:15:55 -0800

    uncomment part one solving

--- 2025/day05/src/main.rs
+++ 2025/day05/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let _test = include_str!("test.txt");
     let _data = get_input(2025, 5);
 
-    // part_one::solve_part_one(&_data);
+    part_one::solve_part_one(&_data);
 
     part_two::solve_part_two(&_data)
 }
